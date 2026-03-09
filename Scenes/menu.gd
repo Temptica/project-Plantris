928c85d3743089f631c463e713f19a42c923108b
Temptica <nic.vanruy@gmail.com>
2026-03-09 17:26:05 +0100

    wip

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var start_button: TextureButton = %StartButton
-@export var settings_button: TextureButton
+@onready var settings_button: TextureButton = %SettingsButton
 
 func  _ready() -> void:
 	start_button.grab_focus()
